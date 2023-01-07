357f0d93a5706eaf0fde2eac73ce73a470317fc6
Mohammed saif <101236642+msaif1996@users.noreply.github.com>
2023-01-07 21:20:01 +0530

    Doc parser commit #3 - parser api

--- src/api/parser.js
+++ src/api/parser.js
@@ -6,7 +6,6 @@ const {verifyAuth } = require('../middleware/auth/verifyAuth')
 const {Router} = require('express')
 const parserRouter = Router()
 
-//pinging parser
 parserRouter.get("/health",verifyAuth,(req,res)=>{
     client.ping()
     .then(function() {
@@ -17,8 +16,31 @@ parserRouter.get("/health",verifyAuth,(req,res)=>{
     })
 })
 
+//fetch all parsers from docparser
 parserRouter.get("/getAllParsers",verifyAuth,(req,res)=>{
     client.getParsers()
+    .then(function(data) {
+        res.status(200).send(data)
+    })
+    .catch(function(err) {
+        res.status(401).send(err)
+    })
+})
+
+//upload file and received parsing id
+parserRouter.post("/uploadFileToParse",verifyAuth,(req,res)=>{
+    client.uploadFileByPath(req.body.parserId,req.body.filePath,req.body.fileName)
+    .then(function(data) {
+        res.status(200).send(data)
+    })
+    .catch(function(err) {
+        res.status(401).send(err)
+    })
+})
+
+//fetch parsed data using parsing id
+parserRouter.post("/getParsedData",verifyAuth,(req,res)=>{
+    client.getResultsByDocument(req.body.parserId,req.body.documentId,"object")
     .then(function(data) {
         res.status(200).send(data)
     })
