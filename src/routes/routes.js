956a8887f2bf9fc2accf65e40fba0bf1c0b06f05
Mohammed saif <101236642+msaif1996@users.noreply.github.com>
2023-01-07 23:17:43 +0530

    Doc parser commit #4 - aws upload api

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const userRouter = require('../api/user');
 const parserRouter = require('../api/parser');
+const awsParserRouter = require('../api/aws')
 const router = express.Router();
 const { verifyAuth } = require("../middleware/auth/verifyAuth");
 
 router.use("/user",userRouter)
 router.use("/parser",parserRouter)
+router.use("/aws",awsParserRouter)
 
 router.get("/health",verifyAuth,(req,res)=>{res.send("Server is up")})
 
